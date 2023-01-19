f3de37f9090a807452401a8cda4f87f3b12f131f
ReddyShekhar <reddysekharroyal17@gmail.com>
2023-01-19 14:55:31 +0530

    Nani

---
+++ SpringMVC/src/main/resources/Nani/com/HelloController.java
@@ -0,0 +1,18 @@
+package Nani.com;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/login")
+	public String userLogin()
+	{
+		return "login";
+	}
+	
+	/*@RequestMapping("/register")
+	public String userReg()
+	{
+		return "register";
+	}*/
+}
